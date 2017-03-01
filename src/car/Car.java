c329832e0b83874ccd46483fe6e06252062b21d4
Martin Rebane <martin@linuxator.com>
2017-03-01 14:03:16 +0200

    Add odometer

--- src/car/Car.java
+++ src/car/Car.java
@@ -4,6 +4,9 @@ public class Car {
 	
 	private int power;
 	private int weight;
+	private Odometer odometer;
+	
+	public static final int ORIGINAL_PRICE = 20000;
 	
 	public double getAvgFuelConsumption() {
 		if (weight < 1000) {
@@ -12,5 +15,10 @@ public class Car {
 		return power / 100 + 30;
 	}
 	
-	
+	public double getValueInEuro() {
+		if (odometer.getMiles() > 100000) {
+			return ORIGINAL_PRICE * 0.9;
+		}
+		return ORIGINAL_PRICE * 0.9 + (100000 - odometer.getMiles())/100;
+	}
 }
---
+++ src/car/Odometer.java
@@ -0,0 +1,8 @@
+package car;
+
+public class Odometer {
+
+	public int getMiles() {
+		throw new UnsupportedOperationException();
+	}
+}
